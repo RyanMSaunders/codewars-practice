43a12c8d928e23649fa965a6a7161c8245b2d2fd
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-10-01 12:15:51 -0400

    Complete Kata even or odd

--- even-or-odd.ts
+++ even-or-odd.ts
@@ -2,3 +2,26 @@
 // Create a function that takes an integer as an argument and returns "Even" 
 // for even numbers or "Odd" for odd numbers.
 
+
+export function evenOrOdd(n:number):string {
+  if (n % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd"
+  }
+  
+}
+
+console.log(evenOrOdd(4));
+console.log(evenOrOdd(3));
+
+
+
+/*
+Returning: string "even" if result is an even number, or string "odd" if result is an odd number
+
+Input: number
+
+
+
+*/
