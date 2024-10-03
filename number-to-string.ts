1dfc1cdf552f1cca4f4eccdd01b0cde189221876
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-10-03 10:26:44 -0400

    Solve number-to-string

---
+++ number-to-string.ts
@@ -0,0 +1,29 @@
+
+export function numberToString(num: number): string {
+  const str = num.toString()
+  return str
+}
+
+console.log(numberToString(76));
+
+
+/**
+ 
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+Returning: a string
+
+Input: a number
+
+Steps: use toStringon number
+
+
+
+ */
