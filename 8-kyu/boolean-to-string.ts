83d2dc399f82620acc86cc392c815fbdc8ec79fb
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-10-03 10:38:28 -0400

    Reorganize files and solve boolean-to-string

---
+++ 8-kyu/boolean-to-string.ts
@@ -0,0 +1,24 @@
+export const boolToWord = (bool: boolean): string => {
+  const result = bool ? "Yes" : "No"
+  console.log(result);
+  
+  return result
+
+}
+
+console.log(boolToWord(true));
+
+
+/*
+Complete the method that takes a boolean value and return a 
+"Yes" string for true, or a "No" string for false.
+
+
+returning: string "yes" for true, string "no" for false
+input: boolean
+
+steps: if statement
+
+
+
+*/
